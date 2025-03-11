10d429c39c5d7c8be846ae5e3994733b80aed1d8
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-11 15:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
   // your code here
+	if(num ==0){
+		return 0+'';
+	}else{
+	
+	let result = (num+""+num)
+	return result 
+	}
+	
 }
 const num = parseInt(prompt("Enter a number."));
+
 alert(decimalToBinary(num));
